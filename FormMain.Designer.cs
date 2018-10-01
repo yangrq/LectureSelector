dcaaac30f61b0bb4ce38ca00bda508c954fb60dd
Root <visualstudio2014@live.com>
2018-10-02 00:16:18 +0800

    初次提交

---
+++ FormMain.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace LectureSelector
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelHigh = new System.Windows.Forms.Label();
+            this.labelLow = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonMaxiumum = new System.Windows.Forms.Button();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.timerFade = new System.Windows.Forms.Timer(this.components);
+            this.richTextBoxMain = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // labelHigh
+            // 
+            this.labelHigh.AutoSize = true;
+            this.labelHigh.Font = new System.Drawing.Font("微软雅黑", 32F);
+            this.labelHigh.ForeColor = System.Drawing.Color.Transparent;
+            this.labelHigh.Location = new System.Drawing.Point(283, 169);
+            this.labelHigh.Name = "labelHigh";
+            this.labelHigh.Size = new System.Drawing.Size(50, 57);
+            this.labelHigh.TabIndex = 0;
+            this.labelHigh.Text = "0";
+            this.labelHigh.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelLow
+            // 
+            this.labelLow.AutoSize = true;
+            this.labelLow.Font = new System.Drawing.Font("微软雅黑", 32F);
+            this.labelLow.ForeColor = System.Drawing.Color.Transparent;
+            this.labelLow.Location = new System.Drawing.Point(339, 169);
+            this.labelLow.Name = "labelLow";
+            this.labelLow.Size = new System.Drawing.Size(50, 57);
+            this.labelLow.TabIndex = 1;
+            this.labelLow.Text = "0";
+            this.labelLow.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.AutoSize = true;
+            this.buttonClose.BackColor = System.Drawing.Color.Tomato;
+            this.buttonClose.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkRed;
+            this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonClose.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonClose.Location = new System.Drawing.Point(635, 0);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(30, 29);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonMaxiumum
+            // 
+            this.buttonMaxiumum.AutoSize = true;
+            this.buttonMaxiumum.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonMaxiumum.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonMaxiumum.FlatAppearance.BorderSize = 0;
+            this.buttonMaxiumum.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGreen;
+            this.buttonMaxiumum.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.buttonMaxiumum.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMaxiumum.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonMaxiumum.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonMaxiumum.Location = new System.Drawing.Point(605, 0);
+            this.buttonMaxiumum.Name = "buttonMaxiumum";
+            this.buttonMaxiumum.Size = new System.Drawing.Size(30, 29);
+            this.buttonMaxiumum.TabIndex = 3;
+            this.buttonMaxiumum.Text = "▢";
+            this.buttonMaxiumum.UseVisualStyleBackColor = false;
+            this.buttonMaxiumum.Click += new System.EventHandler(this.buttonMaxiumum_Click);
+            // 
+            // timerMain
+            // 
+            this.timerMain.Interval = 10;
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.BackColor = System.Drawing.Color.Yellow;
+            this.buttonRun.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.buttonRun.FlatAppearance.BorderSize = 0;
+            this.buttonRun.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Orange;
+            this.buttonRun.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold;
+            this.buttonRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRun.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonRun.ForeColor = System.Drawing.Color.Silver;
+            this.buttonRun.Location = new System.Drawing.Point(263, 366);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(150, 30);
+            this.buttonRun.TabIndex = 4;
+            this.buttonRun.Text = "开始";
+            this.buttonRun.UseVisualStyleBackColor = false;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // timerFade
+            // 
+            this.timerFade.Interval = 20;
+            this.timerFade.Tick += new System.EventHandler(this.timerFade_Tick);
+            // 
+            // richTextBoxMain
+            // 
+            this.richTextBoxMain.BackColor = System.Drawing.Color.White;
+            this.richTextBoxMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxMain.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBoxMain.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBoxMain.Location = new System.Drawing.Point(59, 63);
+            this.richTextBoxMain.Name = "richTextBoxMain";
+            this.richTextBoxMain.ReadOnly = true;
+            this.richTextBoxMain.Size = new System.Drawing.Size(194, 108);
+            this.richTextBoxMain.TabIndex = 5;
+            this.richTextBoxMain.Text = "测试文本";
+            this.richTextBoxMain.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(664, 408);
+            this.Controls.Add(this.richTextBoxMain);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.buttonMaxiumum);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelLow);
+            this.Controls.Add(this.labelHigh);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormMain";
+            this.Text = "LectureSelector";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.SizeChanged += new System.EventHandler(this.FormMain_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHigh;
+        private System.Windows.Forms.Label labelLow;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonMaxiumum;
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Timer timerFade;
+        private System.Windows.Forms.RichTextBox richTextBoxMain;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LectureSelector
+{
+    public partial class FormMain : System.Windows.Forms.Form
+    {
+        public int high = 0, low = 0;
+        public bool moved = false;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_SizeChanged(object sender, EventArgs e)
+        {
+            if (!moved)
+            {
+                labelHigh.Left = Width / 2 - labelHigh.Width;
+                labelLow.Left = Width / 2;
+                labelHigh.Top = Height / 2 - labelHigh.Height;
+                labelLow.Top = Height / 2 - labelLow.Height;
+            }
+            buttonClose.Left = Width - buttonClose.Width;
+            buttonClose.Top = 0;
+            buttonMaxiumum.Left = Width - buttonClose.Width - buttonMaxiumum.Width;
+            buttonMaxiumum.Top = 0;
+            buttonRun.Left = Width / 2 - buttonRun.Width / 2;
+            buttonRun.Top = Height - buttonRun.Height - Height / 20;
+            richTextBoxMain.Height = 4 * (Height - buttonClose.Height) / 5;
+            richTextBoxMain.Width = 4 * Width / 5;
+            richTextBoxMain.Left = Width / 2 - richTextBoxMain.Width / 2;
+            richTextBoxMain.Top = Height / 2 - richTextBoxMain.Height / 2;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void buttonMaxiumum_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            high = rnd.Next(0,9);
+            low = rnd.Next(0, 9);
+            labelHigh.Text = high.ToString();
+            labelLow.Text = low.ToString();
+        }
+
+        public bool state = false;
+        public int number = 88;
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            if (state)
+            {
+                labelHigh.Text = ((number - number % 10) / 10).ToString();
+                labelLow.Text = (number % 10).ToString();
+                timerMain.Stop();
+                state = false;
+                buttonRun.Visible = false;
+                timerFade.Start();
+            }
+            else
+            {
+                timerMain.Start();
+                state = true;
+                buttonRun.Text = "结束";
+            }
+        }
+
+        const int Guying_HTLEFT = 10;
+        const int Guying_HTRIGHT = 11;
+        const int Guying_HTTOP = 12;
+        const int Guying_HTTOPLEFT = 13;
+        const int Guying_HTTOPRIGHT = 14;
+        const int Guying_HTBOTTOM = 15;
+        const int Guying_HTBOTTOMLEFT = 16;
+        const int Guying_HTBOTTOMRIGHT = 17;
+
+        private void timerFade_Tick(object sender, EventArgs e)
+        {
+            labelHigh.Visible = false;
+            labelLow.Visible = false;
+
+            labelHigh.Font = new Font("微软雅黑", 18);
+            labelLow.Font = new Font("微软雅黑", 18);
+
+            labelHigh.Left = 5;
+            labelLow.Left = labelHigh.Width + 5;
+            labelHigh.Top = 3;
+            labelLow.Top = 3;
+
+            labelHigh.Visible = true;
+            labelLow.Visible = true;
+
+            moved = true;
+
+            richTextBoxMain.Visible = true;
+
+            timerFade.Stop();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+            while (true)
+            {
+                number = (new Random(Guid.NewGuid().GetHashCode())).Next(0, 100);
+                var path = ".\\new\\" + number.ToString() + ".txt";
+                if (File.Exists(path))
+                {
+                    richTextBoxMain.Text = File.OpenText(path).ReadToEnd();
+                    File.Move(path, ".\\old\\" + number.ToString() + ".txt");
+                    break;
+                }
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x0084:
+                    base.WndProc(ref m);
+                    Point vPoint = new Point((int)m.LParam & 0xFFFF,
+                        (int)m.LParam >> 16 & 0xFFFF);
+                    vPoint = PointToClient(vPoint);
+                    if (vPoint.X <= 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)Guying_HTTOPLEFT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)Guying_HTBOTTOMLEFT;
+                        else m.Result = (IntPtr)Guying_HTLEFT;
+                    else if (vPoint.X >= ClientSize.Width - 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)Guying_HTTOPRIGHT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)Guying_HTBOTTOMRIGHT;
+                        else m.Result = (IntPtr)Guying_HTRIGHT;
+                    else if (vPoint.Y <= 5)
+                        m.Result = (IntPtr)Guying_HTTOP;
+                    else if (vPoint.Y >= ClientSize.Height - 5)
+                        m.Result = (IntPtr)Guying_HTBOTTOM;
+                    break;
+                case 0x0201:                //鼠标左键按下的消息  
+                    m.Msg = 0x00A1;         //更改消息为非客户区按下鼠标  
+                    m.LParam = IntPtr.Zero; //默认值  
+                    m.WParam = new IntPtr(2);//鼠标放在标题栏内  
+                    base.WndProc(ref m);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        
+    }
+}
